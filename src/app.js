b8d9e53024bbd8da66a0d921f710a17989e27c81
pushhhkar <2003pushkarkumar@gmail.com>
2026-01-25 14:13:35 +0530

    exploring http methods

--- src/app.js
+++ src/app.js
@@ -2,12 +2,27 @@ const express = require('express');
 
 const app = express();
 
+// This will only handle GET call to /user
+app.get("/user", (req, res) => {
+    res.send({firstname: "Pushkar", lastname: "Kumar"});
+});
+
+app.post("/user", (req, res)=>{
+    //saving data to database
+    res.send("data saved successfully");
+})
+
+app.delete("/user", (req, res)=>{
+    res.send("data deleted succesfully");
+})
+
+
+
+//this will match all the HTTP method API calls to /teset
 app.use("/test",(req, res) => {
     res.send("Hello from server");
 });
-app.use((req, res) => {
-    res.send("Try test");
-});
+
 
 
 app.listen(3000, () => {
