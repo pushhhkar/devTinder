28d6129c528678816b969894005e046701a7f83b
pushhhkar <2003pushkarkumar@gmail.com>
2026-01-27 01:38:17 +0530

    GET apis

--- src/app.js
+++ src/app.js
@@ -3,13 +3,34 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
+app.get("/user", async (req, res) =>{
+    const userEmail = req.body.emailId;
+
+    try {
+        const users = await User.find({emailId: userEmail});
+        if(!users){
+            res.status(404).send("user not found");
+        }else{
+            res.send(users);
+        }
+    }catch(err){
+        res.status(400).send("something went wrong");
+    }
+});
+
+app.get("/feed", async (req, res)=>{
+    try {
+        const users = await User.find({});
+        res.send(users);
+    }catch(err){
+        res.status(400).send("Sonething went wrong")
+    }
+})
+
 app.post("/signup", async (req, res)=>{
-    const user = new User({
-        firstName: "achin",
-        lastName: "endulkar",
-        emailId: "achin@endulkar.com",
-        password: "achin@123"
-    });
+    const user = new User(req.body);
 
     try {
         await user.save();
